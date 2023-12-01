ca5c16a574c5f2d723d14fa939aba6a5853547d5
Ali <ali.j7x@gmail.com>
2023-12-01 12:08:43 +0330

    Initial commit

---
+++ android/app/src/main/java/com/example/climbing/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.climbing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = ["img/welcome_one.png", "img/welcome_two.jpg", "img/welcome_three.jpg"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: 3,
+        itemBuilder: (_, index) {
+          return Container(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage(images[index]),
+              fit: BoxFit.cover),
+            ),
+
+          );
+        },
+      ),
+    );
+  }
+}
