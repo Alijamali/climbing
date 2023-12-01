1d58d78ce8d360dd96e7fbd657033678cf0c69bd
Ali <ali.j7x@gmail.com>
2023-12-01 12:16:34 +0330

    a1

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -26,7 +26,7 @@ class _WelcomePageState extends State<WelcomePage> {
             ),
             child: Container(
 
-              child: Text('asd'),
+              child: Text('ali'),
             ),
 
           );
